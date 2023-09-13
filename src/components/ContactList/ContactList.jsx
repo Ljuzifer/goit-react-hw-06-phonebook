3a36e58f4facfdbabbbd7b668c4606d35d7d8f91
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-09-14 01:07:23 +0300

    xxx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -6,6 +6,7 @@ import { List } from './ContactList.styled';
 export const ContactList = () => {
   const contacts = useSelector(getContacts);
   const filterField = useSelector(getFilter);
+
   const visibleContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(filterField.toLowerCase())
   );
